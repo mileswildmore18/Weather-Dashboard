4f8d8dde6621487850f0759b99c373baacda20bc
Miles Wildmore <mileswildmore18@gmail.com>
2024-04-12 20:28:08 -0400

    Added a index, CSS and JavaScript file to get a headstart for the 5 day weather forecast. Also added starting variables on the JavaScript with pseudocode with the outline of what functions I want to use for the search for a location ot work.

---
+++ Assets/js/script.js
@@ -0,0 +1,24 @@
+//Create an API Key
+//Refer to the website the API key was founded with a 5 day forecast
+//Create a search bar
+let searchList = JSON.parse(localStorage.getItem("searches"));
+let nextSearch = JSON.parse(localStorage.getItem("new-search"));
+const  APIKey = "6e186993d5c7985aedd789da8065fa4d";
+const searchBar = document.querySelector(".search")
+const searchCity = document.querysel 
+
+
+
+//Add variable to the search bar
+function searchBar() {
+//Search for a city/code
+    console.log ("search")
+
+    let
+}
+//Store the information that was founded into the localStorage.
+//L
+//Create a search bar for a city, zip code or country
+
+
+

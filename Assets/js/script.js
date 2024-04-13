41b369d1b56edc4ccf178d052ddf4e551def8a80
Miles Wildmore <mileswildmore18@gmail.com>
2024-04-12 22:15:04 -0400

    Added the variables and the event listener for when the search bar is activated on the Weather Dashboard website based on the OpenWeather website with the API key.

--- Assets/js/script.js
+++ Assets/js/script.js
@@ -4,18 +4,40 @@
 let searchList = JSON.parse(localStorage.getItem("searches"));
 let nextSearch = JSON.parse(localStorage.getItem("new-search"));
 const  APIKey = "6e186993d5c7985aedd789da8065fa4d";
-const searchBar = document.querySelector(".search")
-const searchCity = document.querysel 
+const cityCardTemplate = document.querySelector("[data-city-template]");
+const cityCardContainer = document.querySelector("[data-city-cards-container]");
+const searchInput = document.querySelector("[data-search]");
+const searchBar = document.querySelector(".search");
+const searchCity = document.querySelector("city");
 
+let cities = []
 
+searchInput.addEventListener("input", (e));
+const value = e.target.value.toLowerCCase()
+cities.forEach(city => {
+    const isVisible = city.name.toLowerCase.includes(value)
+    city.element.classList.toggle("hide", !isVisible)
+})
+console.log(cities);
 
 //Add variable to the search bar
 function searchBar() {
 //Search for a city/code
-    console.log ("search")
 
-    let
-}
+
+    
+fetch("https://api.openweathermap.org/data/2.5/weather?lat={lat}&lon={lon}&appid=6e186993d5c7985aedd789da8065fa4d)
+    .then (res => res.json())
+    .then(data => {
+        city = data.map(city => {
+        const card = cityCardTemplate.textContent.cloneNode(true).children[0]
+        const body = card.querySelector("[data-body]")
+        body.textContent = city.name
+       cityCardContainer.append(card)
+       return {city: city.name, element: card}
+       }) 
+})}
+//Make the search bar activate on click and provide information if found or not.
 //Store the information that was founded into the localStorage.
 //L
 //Create a search bar for a city, zip code or country

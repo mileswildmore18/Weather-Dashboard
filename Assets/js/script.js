ec0ee58c996f53a5851f9bdf840e0dfe78bd70d3
Miles Wildmore <mileswildmore18@gmail.com>
2024-04-13 16:26:14 -0400

    added variables where my API key gets results from the OpenWeather webpage to bring back data what it could find.

--- Assets/js/script.js
+++ Assets/js/script.js
@@ -3,40 +3,50 @@
 //Create a search bar
 let searchList = JSON.parse(localStorage.getItem("searches"));
 let nextSearch = JSON.parse(localStorage.getItem("new-search"));
-const  apiKey = "6e186993d5c7985aedd789da8065fa4d";
-const urlWeb = "https://api.openweathermap.org"
+const APIKey = "6e186993d5c7985aedd789da8065fa4d";
+let city;
+let urlWeb = "https://api.openweathermap.org/"
 const cityCardTemplate = document.querySelector("[data-city-template]");
 const cityCardContainer = document.querySelector("[data-city-cards-container]");
 const searchInput = document.querySelector("[data-search]");
 const searchBarInput = document.querySelector("#search");
 const searchCity = document.querySelector("city");
 
-let cities = [];
+
 //Add variable to the search bar
 function search() {
+    let input = document.getElementById("search").value;
+    let resultsDiv = document.getElementById("results");
+    let myApiKey = "6e186993d5c7985aedd789da8065fa4d"
+    let apiUrl = "https://api.openweathermap.org/" + encodeURIComponent(input); + '&apikey=' + myApiKey;
+
+    //Make an API request
+    fetch(apiUrl)
+        .then(response => response.json())
+        .then(data => {
+            //Generate API response
+            if(data.results.length > 0) {
+                //Display the search results
+                resultsDiv.innerHTML = "City found: " + data.results[0].name; //In case API returns city names         
+            } else {
+                //Display not found message
+                resultsDiv.innerHTML = "City not found";
+            }//Search results for city or city not found
+        })
+        .catch(error => {
+            console.error('Error fetching data', error);
+            resultsDiv.innerHTML = "An error occured while fetching data";
+        });
+}
 
 
-    if ()
-})
-//Search results for city or city not found
 
 
     
 
 
 
-function fetch(urlWeb);
-    .then (res => res.json())
-    .then(data => {
-        city = data.map(city => {
-        const card = cityCardTemplate.textContent.cloneNode(true).children[0]
-        const body = card.querySelector("[data-body]")
-        body.textContent = city.name
-       cityCardContainer.append(card)
-       return {city: city.name, element: card }
-       }) 
-})
-}
+
 //Make the search bar activate on click and provide information if found or not.
 //Store the information that was founded into the localStorage.
 //L

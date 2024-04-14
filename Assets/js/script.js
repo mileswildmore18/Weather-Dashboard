83111c7150797b988abdfb09ac9762ca9c376cc3
Miles Wildmore <mileswildmore18@gmail.com>
2024-04-14 12:29:14 -0400

    Added a function to get a response for when a city gives back the information on the weather as of today. Also added the informaton gathered into an array to save for later.

--- Assets/js/script.js
+++ Assets/js/script.js
@@ -4,7 +4,7 @@
 let searchList = JSON.parse(localStorage.getItem("#results"));
 let nextSearch = JSON.parse(localStorage.getItem("new-search"));
 let apiKey = "6e186993d5c7985aedd789da8065fa4d";
-
+let city;
 
 const searchInput = document.getElementById('search');
 
@@ -27,7 +27,6 @@ const formSubmitHandler = function (event) {
 
 function search(city) {
     const apiUrl = `https://api.openweathermap.org/data/2.5/weather?q=${city}&appid=${apiKey}`;
-    console.log(apiUrl);
     //fetch api
     fetch(apiUrl)
     .then(function (response) {
@@ -42,6 +41,7 @@ function search(city) {
             response.json()
                 .then(function (data) {
                     console.log(data)
+                    get5DayForecast(city)
                     //send the data to the display function
                     //displayWeather(data)
                     //forecastSearch(city)
@@ -51,7 +51,7 @@ function search(city) {
             alert(`Error: ${response.status.statusText}`);
         }
     })
-
+}
     const displayWeather = function (data) {
         const cityNameEl = document.createElement('h2');
         const cityTempEl = document.createElement('h3');
@@ -64,47 +64,106 @@ function search(city) {
         const cityTemp = (data.main.temp - 273.15) * (9 / 5) + 32;
         cityTempEl.textContent = `Temp: ${cityTemp.toFixed(2)} F`;
         cityWindEl.textContent = `Wind: ${data.wind.speed} MPH`;
-        cityHumidityEl.textContent = `Humidity ${data.humidity} %`
+        cityHumidityEl.textContent = `Humidity ${data.main.humidity} %`;
 
         weatherElement.appendChild(cityNameEl);
         weatherElement.appendChild(cityTempEl);
         weatherElement.appendChild(cityWindEl);
         weatherElement.appendChild(cityHumidityEl);
+
+    const forecastSearch = function(city) {
+        const queryURL = `https://api.openweathermap.org/data/2.5/forecast?q=${city}&appid=${apiKey}`;
+
+        fetch(queryURL)
+        .then(function (response) {
+            //check for response if it returned successfully
+        })
+    }
+        
+}
+function displayForecast (data){
+    for (let i = 1; 1 <= 5; i++) {
+        const forecastCard = document.createElement ('div')
+        forecastCard.setAttribute('id', 'card')
+        
+        const cityDateEl = document.createElement('h4')
+        const iconEl = document.createElement ('h5')
+        const cityTempEl = document.createElement ('h5')
+        const cityWindEl = document.createElement ('h5')
+        const cityHumidityEl = document.createElement ('h5')
+
+        const date = forecastDate(i)
+        const icon = forecastIcon(date, i)
+
+        cityDateEl.textcontent = `${date}`;
+        iconEl.textContent = icon;
+        const cityTemp = (data.list[i].main.temp - 273.15) * (9 / 5) + 32;
+        cityTempEl.textContent = `Temp: ${cityTemp.toFixed(2)} F`;
+        cityWindEl.textContent = `Wind: ${data.list[i].wind.speed} MPH`;
+        cityHumidityEl.textContent = `Humidity ${data.list[i].main.humidity}%`;
+
+        forecastCard.appendChild(cityDateEl);
+        forecastCard.appendChild(iconEl);
+        forecastCard.appendChild(cityTempEl);
+        forecastCard.appendChild(cityWindEl);
+        forecastCard.appendChild(cityHumidityEl);
+        forecastElement.appendChild(forecastCard);
     }
 }
 
-// let apiUrls = `https://api.openweathermap.org/data/2.5/forecast?q=${city}&appid=` + APIKey;
-function get5DayForecast() {
-    // fetch(apiUrls)
-    //     .then(response => response.json())
-    //     .then(data => {
-    //         // Extract relevant weather information for the next 5 days
-    //         const forecasts = data.list.filter((forecast, index) => index % 8 === 0); // Get forecasts for every 8th element (5-day forecast)
+const forecastDate = function (i) {
+    let today = dayjs();
+    let forecastDay = today.add(i, 'day').format('MM/DD/YYYY');
+    return forecastDay;
+}
 
-    //         // Update HTML elements with weather information
-    //         const forecastContainer = document.getElementById('forecastContainer');
-    //         forecasts.forEach(forecast => {
-    //             const date = new Date(forecast.dt * 1000); // Convert timestamp to date
-    //             const temperature = Math.round(forecast.main.temp - 273.15); // Convert from Kelvin to Celsius
-    //             const weatherDescription = forecast.weather[0].description;
 
-    //             // Create HTML elements to display forecast information
-    //             const forecastElement = document.createElement('div');
-    //             forecastElement.classList.add('forecast');
-    //             forecastElement.innerHTML = `
-    //                 <p>Date: ${date.toDateString()}</p>
-    //                 <p>Temperature: ${temperature}°C</p>
-    //                 <p>Weather: ${weatherDescription}</p>
-    //             `;
-    //             forecastContainer.appendChild(forecastElement);
-    //         });
-    //     })
-    //     .catch(error => {
-    //         console.error('Error fetching weather data:', error);
-    //         // Display error message on the website
-    //         document.getElementById('weatherForecast').innerText = 'Error fetching weather data. Please try again later.';
-    //     });
-        // Call the get5DayForecast function to fetch and display the 5-day weather forecast when the page loads
+
+
+
+function get5DayForecast(city) {
+    let apiUrls = `https://api.openweathermap.org/data/2.5/forecast?q=${city}&appid=${apiKey}`;
+    fetch(apiUrls)
+        .then(function (response) {
+            if (response.ok) {
+                response.json()
+                .then(function (data) {
+                    console.log(data)})
+                
+                 } else {
+                //send error alert if response failed
+                alert(`Error: ${response.status.statusText}`);
+            }
+        });
+    }
+            // Extract relevant weather information for the next 5 days
+//             const forecasts = data.list.filter((forecast, index) => index % 8 === 0); // Get forecasts for every 8th element (5-day forecast)
+
+//             // Update HTML elements with weather information
+//             const forecastContainer = document.getElementById('forecastContainer');
+//             forecasts.forEach(forecast => {
+//                 const date = new Date(forecast.dt * 1000); // Convert timestamp to date
+//                 const temperature = Math.round(forecast.main.temp - 273.15); // Convert from Kelvin to Celsius
+//                 const weatherDescription = forecast.weather[0].description;
+
+//                 // Create HTML elements to display forecast information
+//                 const forecastElement = document.createElement('div');
+//                 forecastElement.classList.add('forecast');
+//                 forecastElement.innerHTML = `
+//                     <p>Date: ${date.toDateString()}</p>
+//                     <p>Temperature: ${temperature}°C</p>
+//                     <p>Weather: ${weatherDescription}</p>
+//                 `;
+//                 forecastContainer.appendChild(forecastElement);
+                
+//             });
+//         })
+//         .catch(error => {
+//             console.error('Error fetching weather data:', error);
+//             // Display error message on the website
+//             document.getElementById('weatherForecast').innerText = 'Error fetching weather data. Please try again later.';
+//         });
+//         //Call the get5DayForecast function to fetch and display the 5-day weather forecast when the page loads
 // window.onload = get5DayForecast;
 // }
 
@@ -116,7 +175,7 @@ const clearDiv = function () {
 const searchHistory = function() {
     searchHistoryElement.innerHTML = ''
 }
-}
+
 
 
 //Make the search bar activate on click and provide information if found or not.
